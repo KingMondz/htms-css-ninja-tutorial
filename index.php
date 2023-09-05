129e3b22950b1f47e8cfc653b02c8a4b53ba74ab
josha Muinde <muindejoshua8@gmail.com>
2023-09-05 09:45:21 +0300

    added new index.php, Previous index.php is now inde4.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style3.css">
+    <title>Css nesting</title>
+</head>
+<body>
+    <div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo delectus cum quaerat necessitatibus commodi? Deleniti exercitationem quaerat, sint libero velit aliquam quisquam numquam! Ipsam aspernatur distinctio saepe quasi fugit ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem aliquid distinctio velit amet accusantium accusamus, enim eum nesciunt quod? Cumque et sint ipsa doloremque atque perferendis cum voluptas eligendi amet.</p>
+        <br>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio quos atque aut blanditiis, fugit numquam possimus consequatur explicabo praesentium nulla, obcaecati, doloremque quis sequi natus quas dolor. Enim, illo architecto! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur tempore excepturi suscipit deserunt, repudiandae aliquam error accusantium esse magni ipsa praesentium deleniti sint aperiam molestiae, autem saepe? Temporibus, sed illo.</p>
+        <br>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut ut veniam enim ipsa aliquid. Possimus numquam dolores voluptatum excepturi veniam qui, temporibus, vero ullam quae repellendus beatae. Aut, molestiae eveniet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus corrupti aliquid voluptatum maiores aspernatur, ullam rerum, laudantium sapiente quis nihil reprehenderit, accusantium id ea excepturi culpa fugit in officia omnis.</p>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis optio quos ullam repudiandae. Asperiores magnam laudantium esse quae ipsam commodi quam ut nostrum. Iste doloremque beatae ratione porro voluptate minima? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel a quidem aperiam unde alias quisquam sit? Temporibus animi distinctio vero saepe laudantium libero labore impedit veritatis! Reprehenderit excepturi corporis sequi?</p>
+        <br>
+        <a href="http://google.com"></a>
+        <a href="http://ilovecoockies.cocogugu"></a>
+    </div>
+</body>
+</html>
---
+++ index4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Css Basics 2</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <h2>This is a h2 block element</h2>
+
+    <span>span tag</span>
+    <em>em tag</em>
+    <a href="#">anchor tag</a>
+    <span>Another span tag</span>
+
+    <h2>The fillowing are pure block elements</h2>
+
+    <div>div ttag div div brrrchaa</div>
+    <div>Another div div banana</div>
+    
+    <h3> a h3 tag mzito</h3>
+    <p>paragrapg tag</p>
+    <ul>
+        <!-- ul maenoz(This has been placed without a li tag around it)Edge dev tools has warned against this though -->
+        <li>li tag in a ul tag</li>
+        <li>another li tag</li>
+    </ul>
+
+</body>
+</html>
